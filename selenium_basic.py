dee21987a8164f0b696db5286488b37bf163bda6
alexey-kurbetyev <alexeykurbetyev.photo@gmail.com>
2020-08-03 16:42:23 +0300

    Done

--- selenium_basic.py
+++ selenium_basic.py
@@ -16,7 +16,7 @@ TEST_DATA = {
 
 driver = webdriver.Chrome()
 driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
-
+time.sleep(5)
 if driver.find_element(By.ID, "at-cv-lightbox-close").size != 0:
     driver.find_element(By.ID, "at-cv-lightbox-close").click()
 
@@ -24,4 +24,15 @@ driver.find_element(By.CSS_SELECTOR, "input#user-message").send_keys(TEST_DATA['
 driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
 
 assert driver.find_element(By.ID, "display").text == TEST_DATA['single_input']
+
+time.sleep(5)
+
+driver.find_element(By.CSS_SELECTOR, "#sum1").send_keys(TEST_DATA['two_input_first'])
+driver.find_element(By.CSS_SELECTOR, "#sum2").send_keys(TEST_DATA['two_input_second'])
+driver.find_element(By.CSS_SELECTOR, "button.btn:nth-child(3)").click()
+
+assert driver.find_element(By.CSS_SELECTOR, "#displayvalue").text == str(TEST_DATA['two_input_first'] + TEST_DATA['two_input_second'])
+time.sleep(3)
+if driver.find_element(By.CSS_SELECTOR, "#displayvalue").text == str(TEST_DATA['two_input_first'] + TEST_DATA['two_input_second']):
+    print("Test OK")
 driver.quit()
--- selenium_form.py
+++ selenium_form.py
@@ -1,6 +1,48 @@
 # Open https://the-internet.herokuapp.com/login
 
 # Please automate next test cases:
-# 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
-# 2. Login with invalid creds and check validation error
+
+
 # 3. Logout from app and assert you successfully logged out
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+VALID_CREDS = {'username_t01': 'tomsmith',
+               'password_t01': 'SuperSecretPassword!' }
+
+INVALID_CREDS = {'username_nrt01': 'test123',
+                 'password_nrt01': 'testpass' }
+
+url = 'https://the-internet.herokuapp.com/login'
+
+#1.Login with valid creds/Logout from app (tomsmith / SuperSecretPassword!) and assert you successfully logged in
+
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "#username").send_keys(VALID_CREDS['username_t01'])
+driver.find_element(By.CSS_SELECTOR, "#password").send_keys(VALID_CREDS['password_t01'])
+driver.find_element(By.CSS_SELECTOR, ".fa").click()
+time.sleep(2)
+assert 'Welcome to the Secure Area' in driver.page_source
+print('Login with valid creds is OK!' )
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, ".button").click()
+assert 'Login Page' in driver.page_source
+print('Logout is OK!')
+driver.quit()
+
+# 2. Login with invalid creds and check validation error
+
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "#username").send_keys(INVALID_CREDS['username_nrt01'])
+driver.find_element(By.CSS_SELECTOR, "#password").send_keys(INVALID_CREDS['password_nrt01'])
+driver.find_element(By.CSS_SELECTOR, ".fa").click()
+time.sleep(3)
+assert 'Your username is invalid!' in driver.page_source
+print('Login with invalid creds is OK!(Validation message is shown)' )
+driver.quit()
